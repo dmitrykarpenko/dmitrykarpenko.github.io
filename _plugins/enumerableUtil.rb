8ce9a535dc2f1844220459286d74860749977dc9
Dmytro Karpenko <dmytro.karpenko@strikersoft.com>
2018-05-02 18:40:37 +0300

    sorted menu of post links added; default index content removed; bulb favicon added

---
+++ _plugins/enumerableUtil.rb
@@ -0,0 +1,8 @@
+# module Jekyll
+#   module EnumerableUtil
+#     def sort_by_date(items)
+#       sorted_items = items.sort_by! { |item| item.date }
+#       sorted_items
+#     end
+#   end
+# end
---
+++ _posts/2018-05-01-another-page.markdown
@@ -0,0 +1,14 @@
+---
+layout: default
+title: "Another page 01"
+menus:
+  posts:
+    date: 2018-05-01
+---
+
+## Welcome to another page
+### 05-01
+
+_yay_
+
+[home](/)
